8e1a1adfbc16456c6bf2c0d2178b0b3392b26887
Yehudit10 <yehudit50402@gmail.com>
2025-04-29 22:05:18 +0300

    continuing counsler

--- client/src/features/counsler/AvailableCounsler.jsx
+++ client/src/features/counsler/AvailableCounsler.jsx
@@ -3,7 +3,7 @@ import { useGetCounslersQuery } from "../users/userApiSlice"
 const AvailableCounsler=()=>{
 const {counslers}=useGetCounslersQuery()
 return (
-    
+   <></> 
 )
 }
 export default AvailableCounsler
